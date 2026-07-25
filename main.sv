ee437106da6445c6233ce3ba58dcd4df7c1fbb8c
Daniel <dsl79@case.edu>
2026-07-24 23:36:35 -0400

    moving from .idea

--- main.sv
+++ main.sv
@@ -1,5 +1,5 @@
 // -------------------------------------------------------------------
-// @author danie
+// @author Daniel Lim
 // @copyright (C) 2026, <COMPANY>
 //
 // Created : 24. Jul 2026 11:13 PM
